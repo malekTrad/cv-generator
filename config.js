e711d735103559d007992599826a468e3105b766
Malek Trad <cdm.dti2021@gmail.com>
2022-01-11 09:38:54 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    siteTitle: 'Malek Trad CV', // <title>
+    manifestName: 'Resume',
+    manifestShortName: 'Landing', // max 12 characters
+    manifestStartUrl: '/',
+    manifestBackgroundColor: '#663399',
+    manifestThemeColor: '#663399',
+    manifestDisplay: 'standalone',
+    manifestIcon: 'src/assets/img/website-icon.png',
+    pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+    firstName: 'Malek',
+    lastName: 'Trad',
+    // social
+    socialLinks: [{
+            icon: 'fa-github',
+            name: 'Github',
+            url: 'https://github.com/malekTrad',
+        },
+        {
+            icon: 'fa-linkedin-in',
+            name: 'Linkedin',
+            url: 'https://linkedin.com/in/malek-trad/',
+        },
+    ],
+    email: 'malek.trad5@gmail.com',
+    phone: '000-00000',
+    address: 'Zaghouan',
+};
